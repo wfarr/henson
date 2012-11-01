0f5a165894c65c1ed3dc52aecf28dd2972c1871a
Will Farrington <wfarr@github.com>
2012-11-01 10:15:20 -0400

    Add debug log level for UI

--- spec/henson/ui_spec.rb
+++ spec/henson/ui_spec.rb
@@ -5,24 +5,61 @@ require 'henson/ui'
 describe Henson::UI do
   let(:ui) { Henson::UI.new Thor::Shell::Basic.new }
 
-  before do
-    Henson.settings.stubs(:[]).with(:quiet).returns(true)
-  end
-
   context "when quiet" do
-    it "info does not send a message" do
-      ui.expects(:log).with("hello").never
-      ui.info "hello"
+    before do
+      Henson.settings.stubs(:[]).with(:quiet).returns(true)
     end
 
-    it "warning sends a message" do
-      ui.expects(:log).with("get worried", :yellow).once
-      ui.warning "get worried"
+    context "when verbose" do
+      before do
+        Henson.settings.stubs(:[]).with(:verbose).returns(true)
+      end
+
+      it "debug sends a message" do
+        ui.expects(:log).with("noize").once
+        ui.debug "noize"
+      end
+
+      it "info does not send a message" do
+        ui.expects(:log).with("hello").never
+        ui.info "hello"
+      end
+
+      it "warning sends a message" do
+        ui.expects(:log).with("get worried", :yellow).once
+        ui.warning "get worried"
+      end
+
+      it "error sends a message" do
+        ui.expects(:log).with("freak out", :red).once
+        ui.error "freak out"
+      end
     end
 
-    it "error sends a message" do
-      ui.expects(:log).with("freak out", :red).once
-      ui.error "freak out"
+    context "when not verbose" do
+      before do
+        Henson.settings.stubs(:[]).with(:verbose).returns(false)
+      end
+
+      it "debug sends a message" do
+        ui.expects(:log).with("noize").never
+        ui.debug "noize"
+      end
+
+      it "info does not send a message" do
+        ui.expects(:log).with("hello").never
+        ui.info "hello"
+      end
+
+      it "warning sends a message" do
+        ui.expects(:log).with("get worried", :yellow).once
+        ui.warning "get worried"
+      end
+
+      it "error sends a message" do
+        ui.expects(:log).with("freak out", :red).once
+        ui.error "freak out"
+      end
     end
   end
 
@@ -33,19 +70,56 @@ describe Henson::UI do
       Henson.settings.stubs(:[]).with(:quiet).returns(false)
     end
 
-    it "info sends a message" do
-      ui.expects(:log).with("hello").once
-      ui.info "hello"
+    context "when verbose" do
+      before do
+        Henson.settings.stubs(:[]).with(:verbose).returns(true)
+      end
+
+      it "debug sends a message" do
+        ui.expects(:log).with("noize").once
+        ui.debug "noize"
+      end
+
+      it "info sends a message" do
+        ui.expects(:log).with("hello").once
+        ui.info "hello"
+      end
+
+      it "warning sends a message" do
+        ui.expects(:log).with("get worried", :yellow).once
+        ui.warning "get worried"
+      end
+
+      it "error sends a message" do
+        ui.expects(:log).with("freak out", :red).once
+        ui.error "freak out"
+      end
     end
 
-    it "warning sends a message" do
-      ui.expects(:log).with("get worried", :yellow).once
-      ui.warning "get worried"
-    end
+    context "when not verbose" do
+      before do
+        Henson.settings.stubs(:[]).with(:verbose).returns(false)
+      end
 
-    it "error sends a message" do
-      ui.expects(:log).with("freak out", :red).once
-      ui.error "freak out"
+      it "debug sends a message" do
+        ui.expects(:log).with("noize").never
+        ui.debug "noize"
+      end
+
+      it "info sends a message" do
+        ui.expects(:log).with("hello").once
+        ui.info "hello"
+      end
+
+      it "warning sends a message" do
+        ui.expects(:log).with("get worried", :yellow).once
+        ui.warning "get worried"
+      end
+
+      it "error sends a message" do
+        ui.expects(:log).with("freak out", :red).once
+        ui.error "freak out"
+      end
     end
   end
 end
