a83c88cc28c070139431474491b7c644f54e4698
Will Farrington <wfarr@github.com>
2012-11-03 14:24:03 -0400

    Code coverage for Source::Path

--- spec/henson/source/path_spec.rb
+++ spec/henson/source/path_spec.rb
@@ -1,46 +1,61 @@
 require 'spec_helper'
 
 describe Henson::Source::Path do
-  let(:instance) do
-    Henson::Source::Path.new("test")
+  let(:source) do
+    Henson::Source::Path.new("spec/fixtures/modules/foobar")
   end
 
   it "can be instantiated" do
-    instance.should_not be_nil
+    source.should_not be_nil
   end
 
   it "is a subclass of Henson::Source::Generic" do
-    instance.kind_of? Henson::Source::Generic
+    source.kind_of? Henson::Source::Generic
+  end
+
+  context "versions" do
+    it "returns an array that contains version from modulefile" do
+      source.stubs(:version_from_modulefile).returns('1.0.0')
+      source.versions.should eql(['1.0.0'])
+    end
   end
 
   context "valid?" do
     it "returns true if path_exists? is true" do
-      instance.stubs(:path_exists?).returns(true)
-      instance.send(:valid?).should be_true
+      source.stubs(:path_exists?).returns(true)
+      source.send(:valid?).should be_true
     end
 
     it "returns false if path_exists? is false" do
-      instance.stubs(:path_exists?).returns(false)
-      instance.send(:valid?).should be_false
+      source.stubs(:path_exists?).returns(false)
+      source.send(:valid?).should be_false
     end
   end
 
   context "path_exists?" do
     it "returns true if path is defined and is a directory" do
-      source = Henson::Source::Path.new("spec/fixtures/modules/foobar")
       source.send(:path_exists?).should be_true
     end
 
     it "returns false if path is not a directory" do
-      source = Henson::Source::Path.new("/prolly/wont/exist")
+      source.stubs(:path).returns("/not/a/real/path")
       source.send(:path_exists?).should be_false
     end
   end
 
   context 'version_from_modulefile' do
     it 'parses the Modulefile to get the version string' do
-      source = Henson::Source::Path.new('spec/fixtures/modules/foobar')
       source.send(:version_from_modulefile).should == '0.0.1'
+    end
+
+    it "raises ModuleNotFound if path DNE" do
+      source.stubs(:path_exists?).returns(false)
+      lambda {
+        source.send(:version_from_modulefile)
+      }.should raise_error(
+        Henson::ModuleNotFound,
+        "spec/fixtures/modules/foobar"
+      )
     end
   end
 end
