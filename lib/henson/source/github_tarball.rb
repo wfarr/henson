e952fe509b08fc16147b6750116641d011deed74
Tim Sharpe <tim@sharpe.id.au>
2013-04-27 21:52:06 +1000

    TomDoc

--- lib/henson/source/github_tarball.rb
+++ lib/henson/source/github_tarball.rb
@@ -7,8 +7,21 @@ require 'rubygems/package'
 module Henson
   module Source
     class GitHubTarball < Generic
-      attr_reader :name, :repo, :version
+      # Public: Returns the String name of the module.
+      attr_reader :name
 
+      # Public: Returns the String repo of the module.
+      attr_reader :repo
+
+      # Public: Returns the String version number of the module.
+      attr_reader :version
+
+      # Public: Initialise a new Henson::Source::GitHubTarball.
+      #
+      # name        - The String name of the module.
+      # requirement - The String version requirement for the module.
+      # repo        - The String GitHub repository to pull the module from
+      #               (e.g. "puppetlabs/puppetlabs-stdlib").
       def initialize(name, requirement, repo)
         @name = name
         @repo = repo
@@ -16,10 +29,17 @@ module Henson
         @version = resolve_version_from_requirement(@requirement)
       end
 
+      # Public: Check if the module tarball has been cached.
+      #
+      # Returns True if the module tarball exists on disk, otherwise False.
       def fetched?
         tarball_path.file?
       end
 
+      # Public: Cache the module tarball on disk. Any tarballs for previous
+      # versions of this module will be removed.
+      #
+      # Returns nothing.
       def fetch!
         clean_up_old_cached_versions
 
@@ -31,12 +51,19 @@ module Henson
         download_file url, tarball_path.to_path
       end
 
+      # Public: Install the module into the install path. If a version of the
+      # module has already been installed, it will first be removed.
+      #
+      # Returns nothing.
       def install!
         install_path.rmtree if install_path.exist?
         install_path.mkpath
         extract_tarball tarball_path.to_path, install_path.to_path
       end
 
+      # Public: Check if the module has been installed.
+      #
+      # Returns True if the module exists in the install path, otherwise False.
       def installed?
         # TODO: Check if Modulefile exists.  If it exists, get the version
         # and check if it needs installing.  Otherwise, assume uninstalled.
@@ -48,6 +75,13 @@ module Henson
       end
 
       private
+
+      # Internal: Make a call to the GitHub API.
+      #
+      # path - The URI String under https://api.github.com to request (e.g.
+      # /repos/puppetlabs/puppetlabs-stdlib/tags).
+      #
+      # Returns the parsed JSON object (probably a Hash).
       def api_call(path)
         url = "https://api.github.com#{path}"
         if ENV['GITHUB_API_TOKEN']
@@ -72,7 +106,14 @@ module Henson
         end
       end
 
+      # Internal: Query the GitHub API for a list of tag names that look like
+      # version numbers. If the tag name starts with a v (e.g. v0.0.1), it will
+      # be stripped.
+      #
+      # Returns an Array of String version numbers.
       def fetch_versions_from_api
+        Henson.ui.debug "Fetching a list of tag names for #{repo}"
+
         data = api_call("/repos/#{repo}/tags")
         if data.nil?
           raise GitHubAPIError,
@@ -88,7 +129,16 @@ module Henson
         }.compact
       end
 
+      # Internal: Download a file, following redirects.
+      #
+      # source - The String URL to download.
+      # dest   - The String path on disk (including filename) that the file
+      #          should be downloaded to.
+      #
+      # Returns nothing.
       def download_file(source, dest)
+        Henson.ui.debug "Downloading #{source} to #{dest}"
+
         uri = URI.parse(source)
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
@@ -100,6 +150,7 @@ module Henson
           if resp.is_a? Net::HTTPSuccess
             File.open(dest, 'wb') { |f| f.write resp.body }
           elsif ["301", "302"].include? resp.code
+            Henson.ui.debug "Following redirect to #{resp.header['location']}"
             download_file(resp.header['location'], dest)
           else
             raise GitHubDownloadError, "GitHub returned #{resp.code} for #{source}"
@@ -107,7 +158,17 @@ module Henson
         end
       end
 
+      # Internal: Extract a tarball to a specified destination, stripping the
+      # first component from the paths in the tarball.
+      #
+      # tarball - The String path on disk to the tarball.
+      # dest    - The String path on disk to the directory that the tarball
+      #           will be extracted into.
+      #
+      # Returns nothing.
       def extract_tarball(tarball, dest)
+        Henson.ui.debug "Extracting #{tarball} to #{dest}"
+
         Gem::Package::TarReader.new(Zlib::GzipReader.open(tarball)).each do |entry|
           entry_name = entry.full_name.split('/')[1..-1].join('/')
           if entry.file?
@@ -118,6 +179,10 @@ module Henson
         end
       end
 
+      # Internal: Create and return the path where the module tarballs will be
+      # cached.
+      #
+      # Returns the Pathname object for the directory.
       def cache_path
         @cache_path ||= lambda {
           path = Pathname.new(Henson.settings[:cache_path]) + 'github_tarball'
@@ -126,16 +191,27 @@ module Henson
         }.call
       end
 
+      # Internal: Return the path where the tarball for this version of the
+      # module will be stored.
+      #
+      # Returns the Pathname object for the tarball.
       def tarball_path
         @tarball_path ||= cache_path + "#{repo.gsub('/', '-')}-#{version}.tar.gz"
       end
 
+      # Internal: Remove all tarballs for the module from the cache directory.
+      #
+      # Returns nothing.
       def clean_up_old_cached_versions
         Dir["#{cache_path.to_path}/#{repo.gsub('/', '-')}*.tar.gz"].each do |f|
           FileUtils.rm f
         end
       end
 
+      # Internal: Create and return the path that the module will be installed
+      # to.
+      #
+      # Returns the Pathname object for the directory.
       def install_path
         @install_path ||= lambda {
           path = Pathname.new(Henson.settings[:path]) + name
