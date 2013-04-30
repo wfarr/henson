c51fa3c3fbedc79fa32129d4c6a4ae9a2743e263
Will Farrington <wfarr@github.com>
2013-04-30 13:04:51 -0700

    Add tomdocs to Henson

--- lib/henson.rb
+++ lib/henson.rb
@@ -6,18 +6,41 @@ require "henson/settings"
 require "henson/version"
 
 module Henson
+  # Public: Get the active settings.
+  #
+  # Returns a Henson::Settings.
   def self.settings
     @settings ||= Settings.new
   end
 
-  def self.ui=(new_ui)
+  # Internal: Set the Henson UI.
+  #
+  # new_ui - The UI object.
+  #
+  # Returns the UI.
+  def self.ui= new_ui
     @ui = new_ui
   end
 
+  # Internal: Get the Henson UI.
+  #
+  # Returns the UI.
   def self.ui
     @ui
   end
 
+  # Internal: Set the Henson GitHub API client.
+  #
+  # new_client - The Henson::APIClient.
+  #
+  # Returns the Henson::APIClient.
+  def self.api_client= new_client
+    @api_client = new_client
+  end
+
+  # Internal: Get the Henson API client or create one if it does not exist.
+  #
+  # Returns the Henson::APIClient.
   def self.api_client
     @api_client ||= Henson::APIClient.new "api.github.com",
       :access_token => ENV["GITHUB_API_TOKEN"]
