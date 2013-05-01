4dd8551210d160fa13e50f7c8b00fc9286bfa7ec
Will Farrington <wfarr@github.com>
2013-04-30 19:16:58 -0700

    Add Henson::API::PuppetForgeClient#versions_for_module

--- lib/henson/api/puppet_forge_client.rb
+++ lib/henson/api/puppet_forge_client.rb
@@ -3,6 +3,33 @@ require "henson/api/client"
 module Henson
   module API
     class PuppetForgeClient < Client
+      # Private: Initializes the local client cache of API responses.
+      def after_initialize
+        @cache = {}
+      end
+
+      # Public: List all candidate versions for a module.
+      #
+      # mod     - The String name of the module in the form <owner>/<name>
+      # options - The optional Hash request options.
+      #
+      # Returns an Array of versions as Strings.
+      def versions_for_module mod, options = {}
+        get_module(mod)["releases"].map { |r| r["version"] }
+      end
+
+      # Internal: Retrieve a Puppet module via the Forge API.
+      #
+      # mod     - The String name of the module in the form <owner>/<name>
+      # options - The optional Hash request options.
+      #
+      # Returns a Hash of the module metadata.
+      def get_module mod, options = {}
+        @cache[mod] ||= request :get, mod, options
+      rescue Henson::APIError
+        raise Henson::PuppetModuleNotFound,
+          "The forge at #{connection.url_prefix} does not have any module #{mod}!"
+      end
     end
   end
 end
--- spec/henson/api/puppet_forge_client_spec.rb
+++ spec/henson/api/puppet_forge_client_spec.rb
@@ -3,4 +3,45 @@ require "spec_helper"
 require "henson/api/puppet_forge_client"
 
 describe Henson::API::PuppetForgeClient do
+  let(:client) { described_class.new "forge.puppetlabs.com" }
+
+  let(:parsed_response_body) do
+    {
+      "author"    => "wfarr",
+      "full_name" =>"wfarr/osx_defaults",
+      "name"      => "osx_defaults",
+      "desc"      => "Puppet provider for OS X defaults",
+      "releases"  => [
+        { "version" => "0.1.2"},
+        { "version" => "0.1.1"},
+        { "version" => "0.1.0"}
+      ],
+      "tag_list" => ["defaults","osx"]
+    }
+  end
+
+  describe "#versions_for_module" do
+
+  end
+
+  describe "#get_module" do
+    it "returns a Hash of module metadata if the request succeeds" do
+      client.expects(:request).with(:get, "wfarr/osx_defaults", {}).
+        returns(parsed_response_body)
+
+      results = client.get_module "wfarr/osx_defaults"
+
+      expect(results).to be_a(Hash)
+      expect(results["releases"]).to be_a(Array)
+      expect(results["author"]).to eq("wfarr")
+    end
+
+    it "raises an error if the request fails" do
+      client.expects(:request).with(:get, "wfarr/osx_defaults", {}).
+        raises(Henson::APIError)
+
+      expect(lambda { client.get_module "wfarr/osx_defaults" }).to \
+        raise_error(Henson::PuppetModuleNotFound)
+    end
+  end
 end
