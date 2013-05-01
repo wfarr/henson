133a239ac0cd88a3d3a64863bfd426cb47289c20
Will Farrington <wfarr@github.com>
2013-04-30 19:29:29 -0700

    Add #fetched? and #version to Henson::Source::Forge

--- lib/henson/source/forge.rb
+++ lib/henson/source/forge.rb
@@ -20,9 +20,19 @@ module Henson
         @requirement = requirement
       end
 
-      def fetched?;   end
+      # Public: Determine whether the download needs fetched.
+      #
+      # Returns True if the download is not required, False otherwise.
+      def fetched?
+        cache_path.file?
+      end
 
-      def versions; []; end
+      # Public: Determine the version of the module to be installed.
+      #
+      # Returns the String version number.
+      def version
+        @version ||= resolve_version_from_requirement(@requirement)
+      end
 
       def fetch!
         # TODO implement me
@@ -30,6 +40,30 @@ module Henson
 
       def install!
         # TODO implement me
+      end
+
+      # Internal: Fetch a list of all candidate versions from the forge
+      #
+      # Returns an Array of versions as Strings.
+      def versions
+        @versions ||= api.versions_for_module name
+      end
+
+      private
+
+      # Internal: Return the dir where the module tarballs will be cached.
+      #
+      # Returns the Pathname object for the directory.
+      def cache_dir
+        @cache_dir ||= Pathname.new(Henson.settings[:cache_path]) + "forge"
+      end
+
+      # Internal: Return the path where the tarball for this version of the
+      # module will be stored.
+      #
+      # Returns the Pathname object for the tarball.
+      def cache_path
+        @cache_path ||= cache_dir + "#{name.gsub("/", "-")}-#{version}.tar.gz"
       end
     end
   end
--- spec/henson/source/forge_spec.rb
+++ spec/henson/source/forge_spec.rb
@@ -1,7 +1,63 @@
 require "spec_helper"
 
 describe Henson::Source::Forge do
+  subject(:it) { described_class.new("bar/foo", ">= 0", "bar/foo") }
+
   it "can be instantiated" do
     expect(Henson::Source::Forge.new("name", "req", "forge")).to_not be_nil
+  end
+
+  describe "#fetched?" do
+    before do
+      it.expects(:version).returns("1.0.0")
+    end
+
+    it "returns true if the tarball is cached" do
+      it.send(:cache_path).expects(:file?).returns(true)
+
+      expect(it.fetched?).to be_true
+    end
+
+    it "returns false if the tarball is not cached" do
+      it.send(:cache_path).expects(:file?).returns(false)
+
+      expect(it.fetched?).to be_false
+    end
+  end
+
+  describe "#version" do
+    it "should return the resolved version" do
+      it.expects(:resolve_version_from_requirement).with(">= 0").once.
+        returns("1.0.0")
+
+      expect(it.version).to eq("1.0.0")
+    end
+  end
+
+  describe "#cache_dir" do
+    it "should return a Pathname object" do
+      expect(it.send(:cache_dir)).to be_a(Pathname)
+    end
+
+    it "should return the path on disk to the tarball directory" do
+      path = Pathname.new(Henson.settings[:cache_path]) + "forge"
+
+      expect(it.send(:cache_dir)).to eq(path)
+    end
+  end
+
+  describe "#cache_path" do
+    it "should return a Pathname object" do
+      it.expects(:version).once.returns("1.2.3")
+      expect(it.send(:cache_path)).to be_a(Pathname)
+    end
+
+    it "should return the path on disk to the tarball for this module" do
+      path = Pathname.new(Henson.settings[:cache_path]) + "forge"
+      path = path + "bar-foo-1.2.3.tar.gz"
+
+      it.expects(:version).once.returns("1.2.3")
+      expect(it.send(:cache_path)).to eq(path)
+    end
   end
 end
