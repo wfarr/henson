175143dfa9f2f55b264a52bf6774735862b86ec5
Will Farrington <wfarr@github.com>
2013-05-02 17:36:26 -0700

    Move Henson::Source::Generic#extract_tarball to Henson::Source::Tarball

--- lib/henson/source/generic.rb
+++ lib/henson/source/generic.rb
@@ -26,31 +26,6 @@ module Henson
       def installed?
         File.directory? "#{Henson.settings[:path]}/#{name}"
       end
-
-      private
-
-      # Internal: Extract a tarball to a specified destination, stripping the
-      # first component from the paths in the tarball. Utilized by Forge and
-      # GitHubTarball sources only.
-      #
-      # tarball - The String path on disk to the tarball.
-      # dest    - The String path on disk to the directory that the tarball
-      #           will be extracted into.
-      #
-      # Returns nothing.
-      def extract_tarball(tarball, dest)
-        Henson.ui.debug "Extracting #{tarball} to #{dest}"
-
-        Gem::Package::TarReader.new(Zlib::GzipReader.open(tarball)).each do |entry|
-          entry_name = entry.full_name.split("/")[1..-1].join("/")
-
-          if entry.file?
-            File.open("#{dest}/#{entry_name}", "wb") { |f| f.write entry.read }
-          elsif entry.directory?
-            FileUtils.mkdir_p "#{dest}/#{entry_name}"
-          end
-        end
-      end
     end
   end
 end
